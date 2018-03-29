eac352af2e10914fcd275a9e2b0b213b2c25c7a7
niin4 <riekkin@gmail.com>
2018-03-29 12:41:50 +0300

    front end

--- www/main.js
+++ www/main.js
@@ -7,10 +7,14 @@ fetch('http://nfchunt.jelastic.metropolia.fi/games/Player1')
         const game = document.createElement('div');
         game.innerHTML = 
             `<h3>${element.g_name}</h3> 
-            ${element.g_user}<br> 
-            ${element.g_welcometext}<br> 
-            ${element.g_completedtext}<br>
-            ${element.g_shortcode}`;
+            <b>Created by:</b><br/>
+            ${element.g_user}<br/>
+            <b>Shortcode:</b><br/>
+            ${element.g_shortcode}<br/>
+            <b>Game welcome text:</b><br/>
+            ${element.g_welcometext}<br/>
+            <b>Game completed text:</b><br/>
+            ${element.g_completedtext}<br/>`;
         container.appendChild(game);
     });
 })
