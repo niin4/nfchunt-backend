0b433a174b3d9b4ff14abe403a5e906944ae9c22
niin4 <riekkin@gmail.com>
2018-05-11 10:26:51 +0300

    apidoc address

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "0.1.0",
   "description": "NFC Hunt API documentation. Send request params as JSON in request body.",
   "title": "NFC Hunt Api",
-  "url": "https://nfchunt.jelastic.metropolia.fi",
+  "url": "https://iinar.net:8080",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-05-09T14:28:14.683Z",
+    "time": "2018-05-11T07:26:25.950Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
